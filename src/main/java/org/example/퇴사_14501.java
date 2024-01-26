2c9e22114834077a2d1ae82d92dc2b627580c037
dmswl6310 <dmswl6310@gmail.com>
2024-01-26 12:41:18 +0900

    feat: 백준_14501

--- src/main/java/org/example/퇴사_14501.java
+++ src/main/java/org/example/퇴사_14501.java
@@ -10,20 +10,23 @@ class 퇴사_14501 {
         System.setIn(new FileInputStream("res/input.txt"));
         Scanner sc = new Scanner(System.in);
 
-        int n=sc.nextInt();
-        int[] time=new int[n];
-        int[] profit=new int[n];
-        int[] dp=new int[n];
-        for(int i=0;i<n;i++){
-            time[i]=sc.nextInt();
-            profit[i]=sc.nextInt();
+        int n = sc.nextInt();
+        int[] time = new int[n];
+        int[] profit = new int[n];
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            time[i] = sc.nextInt();
+            profit[i] = sc.nextInt();
         }
 
-        for(int i=n-1;i>=0;i--){
-            if(t[i])
-            dp[i]
+        dp[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int first = dp[i + 1];
+            int second = i + time[i] <= n ? dp[i + time[i]] + profit[i] : 0;
+            dp[i] = Math.max(first, second);
         }
 
-        System.out.println(result);
+        System.out.println(dp[0]);
     }
 }
